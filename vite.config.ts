76f6e36968bd93808c1cf90e92ace4f1d1dbe89f
rkarshiev <r.karshiev@digitalsparta.com>
2023-07-02 18:59:00 +0300

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import path from "path";
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +15,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-})
+});
