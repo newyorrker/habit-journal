3c1f3a367d912751f9ad0ecc9c5198b93dff9da6
rkarshiev <r.karshiev@digitalsparta.com>
2023-07-02 18:54:17 +0300

    replace App add prettier add normalize

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import path from "path";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/app"),
+      // "@pages": path.resolve(__dirname, "src/pages"),
+      // "@features": path.resolve(__dirname, "src/features"),
+      // "@widgets": path.resolve(__dirname, "src/widgets"),
+      // "@shared": path.resolve(__dirname, "src/shared"),
+      // "@entites": path.resolve(__dirname, "src/entities"),
+    },
+  },
   plugins: [react()],
 })
